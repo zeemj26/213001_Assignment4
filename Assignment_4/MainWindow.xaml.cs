eed405e4c11ec01e59c0794b3fe709eab623befe
unknown <zee.mj26@hotmail.com>
2023-12-01 10:37:18 +0500

    213001

---
+++ Assignment_4/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_4
+{
+    
+    public partial class MainWindow : Window
+    {
+        public Person Person { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Person = new Person() { FirstName = "Saad", LastName = "Khan", Age = 21 };
+            DataContext = this;
+        }
+    }
+}
